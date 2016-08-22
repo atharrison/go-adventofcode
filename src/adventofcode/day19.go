6cbe5d66fd87bf5aa4b899c2006eb9f9554b2d55
Andrew Harrison <aharrison1@underarmour.com>
2016-08-21 23:02:40 -0500

    Day 19, part 2 solved.

--- src/adventofcode/day19.go
+++ src/adventofcode/day19.go
@@ -25,8 +25,8 @@ func ExecuteDay19(inputfile string) {
 	}
 	fmt.Println("----------")
 	if part2 {
-		stepCount := fabricateMolecule("e", molecule, replacements)
-		//		fmt.Printf("Results: %v\n", results)
+		//		stepCount := fabricateMolecule("e", molecule, replacements)
+		stepCount := deconstructMolecule(molecule, "e", replacements, 0)
 		fmt.Printf("Final Step Count: %v\n", stepCount)
 	}
 }
@@ -40,6 +40,45 @@ type Fabrication struct {
 	currentMolecule string
 	stepCount       int
 	distance        int
+}
+
+func deconstructMolecule(molecule string, endMolecule string, replacementSet []Replacement, stepCount int) int {
+
+	if len(molecule) == len(endMolecule) && molecule == endMolecule {
+		fmt.Printf("Found a result, after %v steps\n", stepCount)
+		return stepCount
+	} else if len(molecule) > 1 {
+		best := -1
+		for _, replacement := range replacementSet {
+			splits := splitsForMoleculeAndMatch(molecule, replacement.Replace)
+			if len(splits) > 0 {
+				fmt.Printf("Splits for %v against %v: %v\n", molecule, replacement, splits)
+				for _, split := range splits {
+					prefix := split[0]
+					suffix := split[1]
+					result := fmt.Sprintf("%s%s%s",
+						prefix,
+						replacement.Match,
+						suffix)
+					fmt.Printf("Reduced %v to %v\n", molecule, result)
+					deconstructedStepCount := deconstructMolecule(result, endMolecule, replacementSet, stepCount+1)
+
+					if best < 0 || deconstructedStepCount < best {
+						best = deconstructedStepCount
+					} else {
+						fmt.Printf("Too deep: %v\n", deconstructedStepCount)
+					}
+				}
+			}
+		}
+
+		fmt.Printf("Returning best of %v\n", best)
+		return best
+	} else {
+		//Degenerate
+		fmt.Printf("Failed to find a result on this path.")
+		return -1
+	}
 }
 
 func fabricateMolecule(startMolecule string, target string, replacementSet []Replacement) int {
