c6f386a5b028cfe951416009804229d34cde744f
Andrew Harrison <aharrison1@underarmour.com>
2016-11-29 23:20:28 -0600

    Day 22 Part 2, Not yet solved.

--- src/adventofcode/day22.go
+++ src/adventofcode/day22.go
@@ -40,20 +40,18 @@ func (w *Wizard) Attack(opponent *Boss, cast Spell) int {
 		return 0 // Ooops...
 	}
 
+	//fmt.Printf("Player Casts %v\n", cast.Name)
 	if cast.Duration > 0 {
 		// Cast Duration spell, then finish
 		// Add duration to list of active durations.
-		//fmt.Printf("Player Casts %v\n", cast.Name)
 		w.ActiveDurations = append(w.ActiveDurations, &DurationSpell{SpellCast: cast, RemainingDuration: cast.Duration})
 		return 0
 	} else {
 		if cast.Damage > 0 {
-			//fmt.Printf("Player Casts %v\n", cast.Name)
 			opponent.Character.HitPoints -= cast.Damage
 			dealt = cast.Damage
 		}
 		if cast.Heal > 0 {
-			//fmt.Printf("Player Casts %v\n", cast.Name)
 			w.Character.HitPoints += cast.Heal
 		}
 	}
@@ -70,10 +68,10 @@ func (w *Wizard) ApplyDurationSpells(opponent *Boss) {
 			w.ArmorPoints = duration.SpellCast.Armor
 		}
 
-		if duration.SpellCast.Heal > 0 {
-			//fmt.Printf("Drain provides %v HP; its timer is now %v.\n", duration.SpellCast.Heal, duration.RemainingDuration-1)
-			w.HitPoints += duration.SpellCast.Heal
-		}
+		//if duration.SpellCast.Heal > 0 {
+		//	//fmt.Printf("Drain provides %v HP; its timer is now %v.\n", duration.SpellCast.Heal, duration.RemainingDuration-1)
+		//	w.HitPoints += duration.SpellCast.Heal
+		//}
 
 		if duration.SpellCast.ManaReplenish > 0 {
 			//fmt.Printf("Recharge provides %v mana; its timer is now %v.\n", duration.SpellCast.ManaReplenish, duration.RemainingDuration-1)
@@ -116,12 +114,12 @@ var Spells = []Spell{
 	Spell{Name: "Drain", ManaCost: 73, Damage: 2, Heal: 2},
 	Spell{Name: "Shield", ManaCost: 113, Armor: 7, Duration: 6},
 	Spell{Name: "Poison", ManaCost: 173, Damage: 3, Duration: 6},
-	Spell{Name: "Recharge", ManaCost: 229, ManaReplenish: 101, Duration: 6},
+	Spell{Name: "Recharge", ManaCost: 229, ManaReplenish: 101, Duration: 5},
 }
 
 var MIN_MANA_COST = 53
 var MAX_SPELL_LIST_SIZE = 50
-var SPELL_SEED_START = int64(10000000000000000)
+var SPELL_SEED_START = int64(1000000000000000000)
 
 type DurationSpell struct {
 	SpellCast         Spell
@@ -135,7 +133,6 @@ func RunSpellPermutations() {
 
 	var spellSeed int64
 	spellSeed = SPELL_SEED_START
-
 	for {
 		// Just iterate, starting at spellSeed,
 		// converting the seed to a number in base len(Spells),
@@ -144,8 +141,12 @@ func RunSpellPermutations() {
 		// Try it, and see if you best any previous attempt.
 		spellSeed++
 		spellList := ConvertToBase(spellSeed, int64(len(Spells)))
-
+		//spellList := []int{4, 2, 1, 3, 0}
 		boss := &Boss{
+			//&Character{
+			//	HitPoints:    14,
+			//	DamagePoints: 8,
+			//},
 			&Character{
 				HitPoints:    51,
 				DamagePoints: 9,
@@ -153,6 +154,10 @@ func RunSpellPermutations() {
 		}
 
 		wizard := &Wizard{
+			//Character: &Character{
+			//	HitPoints: 10,
+			//},
+			//ManaPoints:      250,
 			Character: &Character{
 				HitPoints: 50,
 			},
@@ -164,15 +169,15 @@ func RunSpellPermutations() {
 		//fmt.Printf("Running SpellList %v...\n", spellList)
 		result := RunWizardBattle(wizard, boss, spellList, leastManaSpent)
 		//fmt.Printf("Battle Won? %v Mana Spent: %v\n", result, wizard.ManaSpent)
-		if result == true && leastManaSpent > wizard.ManaSpent {
+		if result == true && leastManaSpent >= wizard.ManaSpent {
 			leastManaSpent = wizard.ManaSpent
 			copy(spellList, bestSpellList)
-			fmt.Printf("Battle Won? %v Mana Spent: %v\n", result, wizard.ManaSpent)
+			fmt.Printf("Battle Won? %v Mana Spent: %v, Spells: %v\n", result, wizard.ManaSpent, spellList)
 		}
 
-		if spellSeed%500 == 0 {
-			fmt.Printf("Best Option spends %v Mana: %v\n", leastManaSpent, spellList)
-		}
+		//if spellSeed%500 == 0 {
+		//	fmt.Printf("Best Option spends %v Mana: %v\n", leastManaSpent, spellList)
+		//}
 	}
 
 }
@@ -202,8 +207,8 @@ func RunWizardBattle(wizard *Wizard, boss *Boss, spellList []int, maxManaToSpend
 			//fmt.Printf("Player won after %v rounds!\n", round)
 			return true
 		}
-		if wizard.ManaPoints < MIN_MANA_COST {
-			//fmt.Printf("Boss won after %v rounds! (Player OOM)\n", round)
+		if wizard.HitPoints <= 0 {
+			//fmt.Printf("Boss won after %v rounds!\n", round)
 			return false
 		}
 
@@ -217,16 +222,28 @@ func RunWizardBattle(wizard *Wizard, boss *Boss, spellList []int, maxManaToSpend
 			//fmt.Printf("Boss won after %v rounds!\n", round)
 			return false
 		}
+		if wizard.ManaPoints < MIN_MANA_COST {
+			//fmt.Printf("Boss won after %v rounds! (Player OOM)\n", round)
+			return false
+		}
+
 	}
-	//fmt.Println("!!!!!!!Battle took too long!!!!!!!")
+	fmt.Printf("!!!!!!!Battle took too long!!!!!!! %v\n", spellList)
 	return false
 }
 
 func ExecutePlayerTurn(wizard *Wizard, boss *Boss, cast Spell, round int) int {
 	//fmt.Printf("--- Player Turn (%v) ---\n", round)
 	wizard.ClearSpellEffects()
-	wizard.ApplyDurationSpells(boss)
+
+	// Part 2
+	wizard.HitPoints -= 1
+	if wizard.HitPoints <= 0 {
+		return 0 //Ooops, dead.
+	}
+
 	//fmt.Printf("\tBoss: %v\n\tPlayer: %v\n", boss, wizard)
+	wizard.ApplyDurationSpells(boss)
 	dealt := wizard.Attack(boss, cast)
 	//fmt.Printf("\t\tPlayer dealt %v Damage\n", dealt)
 
@@ -235,9 +252,9 @@ func ExecutePlayerTurn(wizard *Wizard, boss *Boss, cast Spell, round int) int {
 
 func ExecuteBossTurn(wizard *Wizard, boss *Boss, round int) int {
 	//fmt.Printf("--- Boss Turn   (%v) ---\n", round)
+	//fmt.Printf("\tBoss: %v\n\tPlayer: %v\n", boss, wizard)
 	wizard.ClearSpellEffects()
 	wizard.ApplyDurationSpells(boss)
-	//fmt.Printf("\tBoss: %v\n\tPlayer: %v\n", boss, wizard)
 
 	dealt := boss.AttackWizard(wizard)
 	//fmt.Printf("\t\tBoss dealt %v Damage\n", dealt)
